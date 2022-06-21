9dba725672770f2e76ddb0b56bb2a6de8f57deb5
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-21 20:39:15 +0100

    plugin_macros: add macros to register a notification
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
    
    # Title: 
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- plugin/examples/foo_plugin.rs
+++ plugin/examples/foo_plugin.rs
@@ -1,19 +1,31 @@
 extern crate clightningrpc_plugin;
 
+use clightningrpc_plugin::types::LogLevel;
 use clightningrpc_plugin::{commands::RPCCommand, plugin::Plugin};
 use serde_json::{json, Value};
 
 #[derive(Clone)]
 struct PluginState(());
 
+/// HelloRPC is used to register the RPC method
 #[derive(Clone)]
 struct HelloRPC {}
 
+/// Implementation of the RPC method
 impl RPCCommand<PluginState> for HelloRPC {
     fn call<'c>(&self, _plugin: &mut Plugin<PluginState>, _request: &'c Value) -> Value {
         json!({
             "language": "Hello from rust"
         })
+    }
+}
+
+#[derive(Clone)]
+struct OnChannelOpened {}
+
+impl RPCCommand<PluginState> for OnChannelOpened {
+    fn call_void<'c>(&self, _plugin: &mut Plugin<PluginState>, _request: &'c Value) {
+        _plugin.log(LogLevel::Debug, "A new channel was opened!");
     }
 }
 
@@ -32,6 +44,7 @@ fn main() {
             "An example of command line option",
             false,
         )
+        .register_notification("channel_opened", OnChannelOpened {})
         .clone();
     plugin.start();
 }
--- plugin_macros/src/lib.rs
+++ plugin_macros/src/lib.rs
@@ -175,6 +175,7 @@ fn generate_rpc_method(item: &TokenStream, method_call: &RPCCall) -> String {
 /// use clightningrpc_plugin::add_rpc;
 /// use clightningrpc_plugin::commands::RPCCommand;
 /// use clightningrpc_plugin::plugin::Plugin;
+/// use clightningrpc_plugin_macros::add_plugin_rpc;
 ///
 /// #[rpc_method(
 ///     rpc_name = "foo",
@@ -204,6 +205,166 @@ pub fn add_plugin_rpc(items: TokenStream) -> TokenStream {
     format!(
         "use clightningrpc_plugin::add_rpc;
     add_rpc!({}, {});",
+        input[0],
+        input[2]
+            .to_string()
+            .replace("\"", "")
+            .as_str()
+            .to_case(Case::Pascal)
+    )
+    .parse()
+    .unwrap()
+}
+
+/// The struct where the tools darling store the information
+/// the macros regardng the notification, so we implement the struct where we want to unmarshall
+/// the user information.
+#[derive(FromMeta)]
+struct NotificationMethodMacro {
+    /// rpc_name is the name of the plugin that the user want to use
+    /// regarding the RPC method registered by the plugin.
+    on: String,
+}
+
+/// RPC notification struct contains all the information to generate a struct
+struct RPCNotification {
+    /// The name of the notification specified by the user
+    original_name: String,
+    /// struct name for the command struct
+    struct_name: String,
+    /// function body defined by the user
+    fn_body: String,
+}
+
+/// implementation fo the Display method of the rpc method that give
+/// that convert the struct in the function call in a valid rust syntax.
+impl fmt::Display for RPCNotification {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(formatter, "{}", self.fn_body)
+    }
+}
+
+#[proc_macro_attribute]
+pub fn notification(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // parse the macros attributes
+    let attr_list = parse_macro_input!(attr as AttributeArgs);
+    let args = NotificationMethodMacro::from_list(&attr_list);
+    let macro_args = match args {
+        Ok(args) => args,
+        Err(err) => panic!("{}", err),
+    };
+
+    // parse the item, that in this case need to be a function
+    // declaration.
+    let ast_item: Item = parse(item.clone()).unwrap();
+    let fn_dec = match ast_item {
+        Item::Fn(decl) => decl,
+        _ => panic!("The macros is applied over a not function declaration"),
+    };
+    let notification = generate_notification_call(&macro_args, fn_dec);
+    generate_notification_method(&item, &notification)
+        .parse()
+        .unwrap()
+}
+
+// helper method to generator the RPCCall struct and make the code more readable and cleaner.
+fn generate_notification_call(
+    notification: &NotificationMethodMacro,
+    fun_dec: ItemFn,
+) -> RPCNotification {
+    RPCNotification {
+        original_name: notification.on.to_owned(),
+        // FIXMEL append some suffix
+        struct_name: notification.on.as_str().to_case(Case::Pascal),
+        fn_body: fun_dec.block.into_token_stream().to_string(),
+    }
+}
+
+/// helper method to generate the necessary Rust code to implement
+fn generate_notification_method(item: &TokenStream, method_call: &RPCNotification) -> String {
+    format!(
+        "
+    #[derive(Clone, Default)]
+    struct {}<T> {{
+      // keep the information added in the macros to
+      // help future macros to register the plugin.
+      on_event: String,
+      _phantom: Option<PhantomData<T>>
+    }}
+
+   impl<T> {}<T> {{
+      pub fn new() -> Self {{
+         {}::<T>{{
+             on_event: \"{}\".to_string(),
+             _phantom: None,
+          }}
+      }}
+
+      {}
+
+   }}
+
+
+    impl<T: Clone + 'static> RPCCommand<T> for {}<T> {{
+       fn call_void<'c>(&self, _plugin: &mut Plugin<T>, _request: &'c Value) {{
+           {}
+       }}
+    }}
+",
+        method_call.struct_name,
+        method_call.struct_name,
+        method_call.struct_name,
+        method_call.original_name,
+        item.to_string(),
+        method_call.struct_name,
+        method_call.to_string(),
+    )
+    .to_owned()
+}
+
+/// procedural macros to generate the code to register a RPC method created with the
+/// `rpc_method` procedural macro.
+///
+/// this procedural macro hide the complicity that the user need to learn to register
+/// a rpc method with `rpc_method` and continue to be under magic.
+///
+/// the macros take in input as first parameter the plugin, and as second the name of the
+/// rpc function specified by the user.
+///
+/// ```rust
+/// use plugin_macros::{add_plugin_rpc, rpc_method};
+/// use serde_json::{json, Value};
+///
+/// use clightningrpc_plugin::notification;
+/// use clightningrpc_plugin::commands::RPCCommand;
+/// use clightningrpc_plugin::plugin::Plugin;
+/// use clightningrpc_plugin_macros::plugin_register_notification;
+///
+/// #[notification(on = "rpc_command")]
+/// fn on_rpc(_plugin: Plugin<()>, _request: Value) {
+///     _plugin.log(LogLevel::Info, "received an RPC notification");
+/// }
+///
+/// fn main() {
+///     // as fist step you need to make a new plugin instance
+///     // more docs about Plugin struct is provided under the clightning_plugin crate
+///     let mut plugin = Plugin::new((), true);
+///
+///     // plugin_register_notification macros helper that help to register a notification with the
+///    // event name without worry about the rules of the library :)
+///    plugin_register_notification!(plugin, "rpc_command");
+///
+///     plugin.start();
+/// }
+/// ```
+#[proc_macro]
+pub fn plugin_register_notification(items: TokenStream) -> TokenStream {
+    let input = items.into_iter().collect::<Vec<_>>();
+    // FIXME: improve parsing
+    assert_eq!(input.len(), 3);
+    format!(
+        "use clightningrpc_plugin::register_notification;
+    register_notification!({}, {});",
         input[0],
         input[2]
             .to_string()
