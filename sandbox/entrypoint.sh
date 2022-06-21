e203f135792c496f782043666a2da5d602036f4b
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-21 22:07:37 +0100

    ci: include plugin inside the build system and cln conf
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 ./run-bitcoin.sh
 ./generate-block-bitcoin.sh
+cd code || exit 1
+make
+cd .. || exit 1
 ./run-clightning.sh
 sleep 0.5m # Give the time to c-lightning to sync the network
-cd code
+cd code || exit 1
 echo "Running test with Cargo"
-cargo test
-cd ..
-./stop-clightning.sh
-./stop-bitcoin.sh
-rm -rf **/*.pid **/regtest
+cargo test --workspace
