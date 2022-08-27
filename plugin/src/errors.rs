893aa925bab0188de893162695407e57e416f287
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-08-27 13:14:41 +0100

    plugin: make a complete error struct
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- plugin/src/errors.rs
+++ plugin/src/errors.rs
@@ -5,11 +5,27 @@ use std::fmt;
 #[derive(Debug, Clone, Serialize)]
 pub struct PluginError {
     code: i32,
+    #[serde(rename = "message")]
     mgs: String,
+    data: Option<serde_json::Value>,
+}
+
+impl PluginError {
+    #[allow(dead_code)]
+    fn new(code: i32, msg: &str, data: &Option<serde_json::Value>) -> Self
+    where
+        Self: Sized,
+    {
+        return PluginError {
+            code,
+            mgs: msg.to_string(),
+            data: data.to_owned(),
+        };
+    }
 }
 
 impl fmt::Display for PluginError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "code: {}, msg: {}", self.code, self.mgs)
+        write!(f, "code: {}, msg: {}", self.code, self.mgs,)
     }
 }
