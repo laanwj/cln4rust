2204d127804ee8de6529d3f849bc4b832ed0a439
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-09-19 19:02:26 +0100

    feat(gossip_mapp): add gossip_map implementation
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ gossip_map/src/lib.rs
@@ -0,0 +1,20 @@
+struct GossipMap;
+
+impl GossipMap {
+    // Create a new instance of the gossip map.
+    pub fn new() -> Self {
+        GossipMap {}
+    }
+
+    pub fn get_channel(short_chananel_id: &str) -> Result<(), ()> {
+        Ok(())
+    }
+
+    pub fn get_node(node_id: &str) -> Result<(), ()> {
+        Ok(())
+    }
+
+    pub fn refresh() -> Result<(), ()> {
+        Ok(())
+    }
+}
---
+++ gossip_map/src/types.rs
@@ -0,0 +1,16 @@
+//! Gossip map types implementations.
+
+/// TODO: implement it.
+struct GossipNodeId;
+
+/// TODO: implement it.
+struct GossipNode;
+
+/// TODO: implement it
+struct GossipChannel;
+
+/// TODO: implement it
+struct GossipPartialChannel;
+
+// TODO: implementing it
+struct GossipStoredHeader;
