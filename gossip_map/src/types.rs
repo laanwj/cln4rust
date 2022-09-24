d5118c3b658e99248a88f6feb021b367a070d345
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-09-24 23:17:12 +0100

    gossip_map: impl the gossip map basic types
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- gossip_map/src/types.rs
+++ gossip_map/src/types.rs
@@ -1,16 +1,136 @@
 //! Gossip map types implementations.
+use byteorder::{BigEndian, ReadBytesExt};
+use std::{collections::HashMap, io::BufRead, ops::Deref, str, str::Bytes, vec::Vec};
 
-/// TODO: implement it.
-struct GossipNodeId;
+use crate::flags::{GOSSIP_STORE_LEN_DELETED_BIT, GOSSIP_STORE_LEN_MASK};
 
-/// TODO: implement it.
-struct GossipNode;
+trait GossipType {
+    /// Decode the gossip message from a sequence of bytes.
+    fn decode(stream: &mut dyn BufRead) -> Result<Self, std::io::Error>
+    where
+        Self: Sized;
 
-/// TODO: implement it
-struct GossipChannel;
+    /// Encode the gossip message in a sequence of bytes.
+    fn encode(&self) -> Bytes;
+}
 
-/// TODO: implement it
-struct GossipPartialChannel;
+/// Node Id encoded for the gossip map
+pub struct GossipNodeId {
+    node_id: String,
+}
 
-// TODO: implementing it
-struct GossipStoredHeader;
+impl GossipNodeId {
+    pub(crate) fn from_bytes(buff: &[u8; 32]) -> GossipNodeId {
+        GossipNodeId {
+            node_id: String::from_utf8(buff.to_vec()).unwrap(),
+        }
+    }
+}
+
+impl GossipType for GossipNodeId {
+    fn decode(stream: &mut dyn BufRead) -> Result<Self, std::io::Error>
+    where
+        Self: Sized,
+    {
+        let mut node_id: String = String::new();
+        stream.read_to_string(&mut node_id)?;
+
+        // FIXME: missed sanity check!
+        let res = GossipNodeId { node_id };
+        Ok(res)
+    }
+
+    fn encode(&self) -> Bytes {
+        todo!()
+    }
+}
+
+pub struct GossipNode<'a> {
+    node_id: GossipNodeId,
+    announce_fileds: Option<HashMap<String, String>>,
+    announce_offset: Option<u32>,
+    channels: Vec<&'a GossipChannel<'a>>,
+}
+
+impl<'a> GossipNode<'a> {
+    /// add a gossip channel inside the gossip map.
+    pub fn add_channel(&'a mut self, channel: &'a GossipChannel) {
+        self.channels.push(channel);
+    }
+}
+
+impl GossipType for GossipNode<'_> {
+    fn decode(stream: &mut dyn BufRead) -> Result<Self, std::io::Error>
+    where
+        Self: Sized,
+    {
+        let mut buff = [0; 32];
+        stream.read_exact(&mut buff)?;
+        let node_id = GossipNodeId::from_bytes(&buff);
+        todo!()
+    }
+
+    fn encode(&self) -> Bytes {
+        todo!()
+    }
+}
+
+/// Channel Information stored inside the Gossip Map.
+pub struct GossipChannel<'a> {
+    fileds: HashMap<String, String>,
+    annound_offset: u32,
+    scid: String,
+    node_one: &'a GossipNode<'a>,
+    node_two: &'a GossipNode<'a>,
+    update_fields: Vec<HashMap<String, String>>,
+    update_offset: Vec<u32>,
+    satoshi: Option<u64>,
+    half_channels: Vec<&'a GossipPartialChannel>,
+}
+
+impl GossipType for GossipChannel<'_> {
+    fn decode(stream: &mut dyn BufRead) -> Result<Self, std::io::Error>
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+
+    fn encode(&self) -> Bytes {
+        todo!()
+    }
+}
+
+/// One direction gossip map channel
+pub struct GossipPartialChannel {}
+
+/// Gossip map header, that contains the version
+/// of the gossip map.
+pub struct GossipStoredHeader {
+    flags: bool,
+    len: u16,
+    crc: u32,
+    timestamp: u32,
+}
+
+impl GossipType for GossipStoredHeader {
+    fn decode(stream: &mut dyn BufRead) -> Result<Self, std::io::Error>
+    where
+        Self: Sized,
+    {
+        let len = stream.read_u16::<BigEndian>()?;
+        let crc = stream.read_u32::<BigEndian>()?;
+        let timestamp = stream.read_u32::<BigEndian>()?;
+
+        Ok(GossipStoredHeader {
+            timestamp,
+            crc,
+            flags: (len as u32 & GOSSIP_STORE_LEN_DELETED_BIT) != 0,
+            len: (len & GOSSIP_STORE_LEN_MASK),
+        })
+    }
+
+    fn encode(&self) -> Bytes {
+        todo!()
+    }
+}
