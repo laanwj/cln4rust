4bd558d6440cdef5a21b7237dd4d367d79410426
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-05-12 19:57:01 +0200

    feat: add the possiblity to add a node and a channel
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- gossip_map/src/flags.rs
+++ gossip_map/src/flags.rs
@@ -4,14 +4,13 @@ pub static GOSSIP_STORE_MAJOR_VERSION: u16 = 0 << 5;
 pub static GOSSIP_STORE_MAJOR_VERSION_MASK: u16 = 0xE0;
 
 /// Deleted fields should be ignored: on restart, they will be removed as the gossip_store is rewritten.
-pub static GOSSIP_STORE_LEN_DELETED_BIT: u32 = 0x80000000;
+pub const GOSSIP_STORE_LEN_DELETED_BIT: u16 = 0x8000;
 /// The push flag indicates gossip which is generated locally: this is important for gossip timestamp filtering,
 /// where peers request gossip and we always send our own gossip messages even if the timestamp wasn't within their
-/// request.pub static GOSSIP_STORE_LEN_PUSH_BIT: u32 = 0x40000000;
-pub static GOSSIP_STORE_LEN_RATELIMIT_BIT: u32 = 0x20000000;
+pub const GOSSIP_STORE_LEN_PUSH_BIT: u16 = 0x4000;
 /// The ratelimit flag indicates that this gossip message came too fast.
 /// The message are corded in the gossip map, but don't relay it to peers.
-pub static GOSSIP_STORE_LEN_MASK: u16 = 0x0000FFFF;
+pub const GOSSIP_STORE_LEN_RATELIMIT_BIT: u16 = 0x2000;
 
 // These duplicate constants in lightning/gossipd/gossip_store_wiregen.h
 pub const WIRE_GOSSIP_STORE_PRIVATE_CHANNEL: u16 = 4104;
--- gossip_map/src/types.rs
+++ gossip_map/src/types.rs
@@ -1,9 +1,14 @@
 //! Gossip map types implementations.
+use std::io::Read;
 use std::{collections::HashMap, io::BufRead, str::Bytes, vec::Vec};
 
-use byteorder::{BigEndian, ReadBytesExt};
+use bitcoin::PublicKey;
+use fundamentals::core::FromWire;
+use fundamentals::prelude::bolt7::{ChannelAnnouncement, ChannelUpdate, NodeAnnouncement};
+use fundamentals::types::{Point, ShortChannelId};
+use fundamentals_derive::DecodeWire;
 
-use crate::flags::{GOSSIP_STORE_LEN_DELETED_BIT, GOSSIP_STORE_LEN_MASK};
+use crate::gossip_store_msg::GossipStoreChannelAmount;
 
 trait GossipType {
     /// Decode the gossip message from a sequence of bytes.
@@ -21,136 +26,128 @@ pub struct GossipNodeId {
     pub(crate) node_id: String,
 }
 
-impl GossipNodeId {
-    pub(crate) fn from_bytes(buff: &[u8; 33]) -> std::io::Result<Self> {
-        Ok(GossipNodeId {
-            node_id: String::from_utf8(buff.to_vec()).map_err(|err| {
-                std::io::Error::new(std::io::ErrorKind::Other, format!("{:?}", err))
-            })?,
-        })
-    }
-}
-
-impl GossipType for GossipNodeId {
-    fn decode(stream: &mut dyn BufRead) -> Result<Self, std::io::Error>
-    where
-        Self: Sized,
-    {
-        let mut node_id: String = String::new();
-        stream.read_to_string(&mut node_id)?;
-
-        // FIXME: missed sanity check!
-        let res = GossipNodeId { node_id };
-        Ok(res)
-    }
-
-    fn encode(&self) -> Bytes {
-        todo!()
-    }
-}
-
-pub struct GossipNode<'a> {
-    node_id: GossipNodeId,
-    announce_fileds: Option<HashMap<String, String>>,
-    announce_offset: Option<u32>,
-    channels: Vec<&'a GossipChannel<'a>>,
-}
-
-impl GossipNode<'_> {
-    pub fn new(node_id: GossipNodeId) -> Self {
+impl From<&str> for GossipNodeId {
+    fn from(value: &str) -> Self {
         Self {
-            node_id,
-            announce_fileds: None,
-            announce_offset: None,
-            channels: vec![],
+            node_id: value.to_owned(),
         }
     }
 }
 
-impl<'a> GossipNode<'a> {
-    /// add a gossip channel inside the gossip map.
-    pub fn add_channel(&'a mut self, channel: &'a GossipChannel) {
-        self.channels.push(channel);
+impl GossipNodeId {
+    pub(crate) fn from_bytes(buff: &Point) -> std::io::Result<Self> {
+        Ok(GossipNodeId {
+            node_id: PublicKey::from_slice(buff).unwrap().to_string(),
+        })
     }
 }
 
-impl GossipType for GossipNode<'_> {
-    fn decode(stream: &mut dyn BufRead) -> Result<Self, std::io::Error>
-    where
-        Self: Sized,
-    {
-        let mut buff = [0; 33];
-        stream.read_exact(&mut buff)?;
-        let node_id = GossipNodeId::from_bytes(&buff)?;
-        Ok(Self {
-            node_id,
-            announce_fileds: None,
-            announce_offset: None,
-            channels: vec![],
-        })
-    }
+#[derive(Clone, Debug)]
+pub struct GossipNode {
+    node_id: GossipNodeId,
+    announced: bool,
+    raw_message: Option<NodeAnnouncement>,
+    channels: Vec<GossipChannel>,
+}
 
-    fn encode(&self) -> Bytes {
-        todo!()
+impl GossipNode {
+    pub fn new(node_id: GossipNodeId, inner: Option<NodeAnnouncement>) -> Self {
+        Self {
+            node_id,
+            // FIXME: this can be optional right? for
+            // private channel we do not have one.
+            raw_message: inner,
+            channels: vec![],
+            announced: true,
+        }
+    }
+}
+
+impl GossipNode {
+    /// add a gossip channel inside the gossip map.
+    pub fn add_channel(&mut self, channel: &GossipChannel) {
+        self.channels.push(channel.clone());
     }
 }
 
 /// Channel Information stored inside the Gossip Map.
-pub struct GossipChannel<'a> {
-    fileds: HashMap<String, String>,
+#[derive(Clone, Debug)]
+pub struct GossipChannel {
+    inner: ChannelAnnouncement,
     annound_offset: u32,
-    scid: String,
-    node_one: &'a GossipNode<'a>,
-    node_two: &'a GossipNode<'a>,
+    scid: ShortChannelId,
+    node_one: GossipNodeId,
+    node_two: GossipNodeId,
     update_fields: Vec<HashMap<String, String>>,
     update_offset: Vec<u32>,
     satoshi: Option<u64>,
-    half_channels: Vec<&'a GossipPartialChannel>,
+    half_channels: HashMap<u8, GossipPartialChannel>,
+    private: bool,
 }
 
-impl GossipType for GossipChannel<'_> {
-    fn decode(stream: &mut dyn BufRead) -> Result<Self, std::io::Error>
-    where
-        Self: Sized,
-    {
-        todo!()
+impl GossipChannel {
+    pub fn new(
+        inner: ChannelAnnouncement,
+        node_one: &GossipNodeId,
+        node_two: &GossipNodeId,
+    ) -> Self {
+        GossipChannel {
+            inner: inner.clone(),
+            annound_offset: 0,
+            scid: inner.short_channel_id,
+            // FIXME: I can store only the ID?
+            node_one: node_one.clone(),
+            node_two: node_two.clone(),
+            update_fields: vec![],
+            update_offset: vec![],
+            satoshi: None,
+            half_channels: HashMap::new(),
+            private: false,
+        }
     }
 
-    fn encode(&self) -> Bytes {
-        todo!()
+    pub fn channel_update(&mut self, channel_update: &ChannelUpdate) {
+        // FIXME: check how to normalize the BitFlag
+        let direction = 1;
+        self.half_channels.insert(
+            direction,
+            GossipPartialChannel::new(channel_update.to_owned()),
+        );
+    }
+
+    pub fn set_amount(&mut self, amount: GossipStoreChannelAmount) {
+        self.satoshi = Some(amount.satoshis.into());
+    }
+
+    pub fn set_private(&mut self, private: bool) {
+        self.private = private;
     }
 }
 
 /// One direction gossip map channel
-pub struct GossipPartialChannel {}
+#[derive(Debug, Clone)]
+pub struct GossipPartialChannel {
+    pub inner: ChannelUpdate,
+}
+
+impl GossipPartialChannel {
+    pub fn new(inner: ChannelUpdate) -> Self {
+        Self { inner }
+    }
+}
 
 /// Gossip map header, that contains the version
 /// of the gossip map.
+#[derive(DecodeWire, Debug)]
 pub struct GossipStoredHeader {
-    flags: bool,
-    len: u16,
+    flags: u16,
+    pub len: u16,
     crc: u32,
     timestamp: u32,
 }
 
-impl GossipType for GossipStoredHeader {
-    fn decode(stream: &mut dyn BufRead) -> Result<Self, std::io::Error>
-    where
-        Self: Sized,
-    {
-        let len = stream.read_u16::<BigEndian>()?;
-        let crc = stream.read_u32::<BigEndian>()?;
-        let timestamp = stream.read_u32::<BigEndian>()?;
-
-        Ok(GossipStoredHeader {
-            timestamp,
-            crc,
-            flags: (len as u32 & GOSSIP_STORE_LEN_DELETED_BIT) != 0,
-            len: (len & GOSSIP_STORE_LEN_MASK),
-        })
-    }
-
-    fn encode(&self) -> Bytes {
-        todo!()
+impl GossipStoredHeader {
+    pub fn flag(&self) -> u16 {
+        self.flags
     }
 }
