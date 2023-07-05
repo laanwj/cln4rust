cb111ca281b42de4a14f88617e9f45d4ed4a2468
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-07-05 19:14:42 +0200

    feat(test): Implementing a new test framework for core lightning
    This commit implements a minimal and configurable core lightning test
    framework that allows spinning up a core lightning node and a Bitcoin
    node, and controlling them through the RPC API.
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ testing/src/lib.rs
@@ -0,0 +1,52 @@
+pub mod btc;
+pub mod cln;
+
+pub mod prelude {
+    pub use port_selector as port;
+    pub use tempfile;
+
+    pub use bitcoincore_rpc;
+    pub use clightningrpc;
+
+    pub use crate::check_dir_or_make_if_missing;
+    pub use crate::macros::*;
+}
+
+pub mod macros {
+    #[macro_export]
+    macro_rules! wait_for {
+        ($callback:expr, $timeout:expr) => {
+            use log;
+            use tokio::time::{sleep, Duration};
+
+            for wait in 0..$timeout {
+                if let Err(_) = $callback.await {
+                    sleep(Duration::from_millis(wait)).await;
+                    continue;
+                }
+                log::info!("callback completed in {wait} milliseconds");
+                break;
+            }
+        };
+        ($callback:expr) => {
+            use crate::DEFAULT_TIMEOUT;
+
+            $crate::wait_for!($callback, DEFAULT_TIMEOUT);
+        };
+    }
+
+    pub use wait_for;
+}
+
+static DEFAULT_TIMEOUT: u64 = 100;
+
+pub async fn check_dir_or_make_if_missing(path: String) -> anyhow::Result<()> {
+    use std::path::Path;
+    use tokio::fs::create_dir;
+
+    if !Path::exists(Path::new(&path.to_owned())) {
+        create_dir(path.clone()).await?;
+        log::debug!("created dir {path}");
+    }
+    Ok(())
+}
