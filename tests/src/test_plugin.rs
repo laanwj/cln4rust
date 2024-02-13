8e347cb7b5ae862e65bbf4915cef027f7deeab4a
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-02-13 18:32:03 +0100

    ci: rewrite integration testing
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ tests/src/test_plugin.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+use rstest::*;
+use serde_json::Value;
+
+use clightning_testing::cln;
+
+use crate::fixtures::lightningd;
+
+#[rstest]
+fn plugin_rpc_call_call(lightningd: cln::Node) {
+    let lightningd = lightningd.rpc();
+    let response = lightningd
+        .call::<HashMap<String, Value>, HashMap<String, Value>>("hello", HashMap::new())
+        .unwrap();
+    assert!(response.contains_key("language"));
+}
+
+#[rstest]
+fn plugin_macros_rpc_call_call(lightningd: cln::Node) {
+    let lightningd = lightningd.rpc();
+    let response = lightningd
+        .call::<HashMap<String, Value>, HashMap<String, Value>>("foo_macro", HashMap::new())
+        .unwrap();
+    assert!(response.contains_key("is_dynamic"));
+}
